16218994d8cfe7e767ca8f81cd7b41ef8564543b
G TANG <49752710+coffeeboost@users.noreply.github.com>
2025-10-12 02:27:41 -0400

    Update vite.config.ts
    Signed-off-by: G TANG <49752710+coffeeboost@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], base: '/portfoliov3/',})
+export default defineConfig({ plugins: [react()], base: '/',}) //base: '/portfoliov3/' if not custom domain
